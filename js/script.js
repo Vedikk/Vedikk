dae33dbf28c5fa9b521583c17fb7aea51cea10d2
Vedikk <maystruk94@gmail.com>
2016-11-06 11:37:28 +0200

    2

--- js/script.js
+++ js/script.js
@@ -5,5 +5,9 @@ $(document).ready(function () {
     $(".menu_button").click(function () {
         $('.top_nav').slideToggle();
     });
-
+    $('a[href^="#"]').click(function(){
+        var target = $(this).attr('href');
+        $('html, body').animate({scrollTop: $(target).offset().top}, 900);
+        return false;
+    });
 });
