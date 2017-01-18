bb3b53f5303cd71eaf514fa35fe11f8a6fed8fff
Vedikk <maystruk94@gmail.com>
2017-01-18 20:04:05 +0200

    1

--- js/script.js
+++ js/script.js
@@ -1,10 +1,7 @@
 /**
- * Created by mayst on 30.10.2016.
+ * Created by mayst on 17.01.2017.
  */
 $(document).ready(function () {
-    $(".menu_button").click(function () {
-        $('.top_nav').slideToggle();
-    });
     $('a[href^="#"]').click(function(){
         var target = $(this).attr('href');
         $('html, body').animate({scrollTop: $(target).offset().top}, 900);
